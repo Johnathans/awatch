475e3bf60f9d29999e76cf6c387cea7acb7626c5
Johnathan Smith <smithjohnathanr@gmail.com>
2015-04-09 16:27:35 -0700

    added admin panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   resources :conspiracies
 
   get 'about' => 'pages#about'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150404054301) do
+ActiveRecord::Schema.define(version: 20150409223434) do
 
   create_table "comments", force: true do |t|
     t.integer  "link_id"
@@ -118,6 +118,7 @@ ActiveRecord::Schema.define(version: 20150404054301) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
