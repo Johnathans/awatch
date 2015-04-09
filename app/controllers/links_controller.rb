d91c9dd66893ddc2e0b48035e3ee8bad5474001b
Johnathan Smith <smithjohnathanr@gmail.com>
2015-04-09 13:16:54 -0700

    updated friendly id

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -89,6 +89,6 @@ class LinksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def link_params
-      params.require(:link).permit(:title, :url, :image, :body)
+      params.require(:link).permit(:title, :url, :image, :body, :slug)
     end
 end
