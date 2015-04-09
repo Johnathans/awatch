f837f4c88297b041f932c6e07421544d0e813ea9
Johnathan Smith <smithjohnathanr@gmail.com>
2015-04-09 16:42:37 -0700

    updated rails admin initalizer to work on production

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,6 +1,6 @@
 RailsAdmin.config do |config|
   config.authorize_with do
-    redirect_to main_app.root_path unless warden.user.admin == true
+    redirect_to main_app.root_url unless warden.user.admin == true
   end
 
   ### Popular gems integration
